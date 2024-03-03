695e9116ddfe55aa3ac9c9cd36608be7af759e5f
MonteBro <czech.illusion@gmail.com>
2024-03-03 14:36:46 +0100

    fix: dont check websocket origin

--- constants.go
+++ constants.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/websocket"
 )
 
-// Pub/Sub constant
+// Pub/Sub "router" instance
 var PS = NewPubSub()
 
-// WS Upgrader constant
+// WS Upgrader instance
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all connections
+	},
 }
 
 // ID generator letters constant
