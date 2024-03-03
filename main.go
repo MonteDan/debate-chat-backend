4620f7dd08fb2d270b58163e66545ecdbdc4f638
MonteBro <czech.illusion@gmail.com>
2024-03-03 11:34:17 +0100

    feat: cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -13,11 +14,19 @@ func main() {
 
 	router.HandleFunc("/ws", handleUsing(handleWebSocketConnection, db))
 	router.HandleFunc("/message", handleUsing(handleSendMessage, db)).Methods("POST")
-	router.HandleFunc("/chat", handleUsing(handleCreateChat, db)).Methods("POST")
-	router.HandleFunc("/chat/{id}/admin", handleUsing(handleGetChatAdmin, db)).Methods("GET")
-	router.HandleFunc("/chat/{id}/user", handleUsing(handleGetChatUser, db)).Methods("GET")
+	router.HandleFunc("/chats", handleUsing(handleCreateChat, db)).Methods("POST")
+	router.HandleFunc("/chats/{id}/admin", handleUsing(handleGetChatAdmin, db)).Methods("GET")
+	router.HandleFunc("/chats/{id}", handleUsing(handleGetChatUser, db)).Methods("GET")
 	router.HandleFunc("/message/{id}", handleUsing(handleDeleteMessage, db)).Methods("DELETE")
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+	})
+	corsHandler := c.Handler(router)
+
 	log.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", corsHandler))
 }
